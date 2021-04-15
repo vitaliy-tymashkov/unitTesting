0dcc0455bee5c0030882912bab0390143bffc64a
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2021-04-15 17:24:33 +0200

    Check NPE added

--- src/main/java/com/epam/learn/App.java
+++ src/main/java/com/epam/learn/App.java
@@ -3,12 +3,22 @@ package com.epam.learn;
 import com.epam.learn.service.Range;
 
 public class App {
-
+    private static String tmp;
     public static final String MESSAGE = "Check if %s is in range [%s - %s]: %s";
 
     public static void main(String[] args) {
         int value = 15;
         System.out.println(
                 String.format(MESSAGE, value,  Range.getStartNumber(), Range.getEndNumber(), Range.checkIfValueIsInRange(value)));
+
+        checkNpe();
+    }
+
+    private static void checkNpe() {
+        if ("some text".equals(tmp)){
+            System.out.println("Null Pointer not happened - will not execute");
+        } else {
+            System.out.println("Null Pointer not happened");
+        }
     }
 }
